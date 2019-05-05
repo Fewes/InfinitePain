6181cda1dc6ca2b6e3f49bb75f0226106c433eb4
Sven <sven_thole@hotmail.com>
2019-05-05 13:21:17 +0200

    Text scripts

---
+++ Assets/ScoreScreen.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ScoreScreen : MonoBehaviour
+{
+    Text m_Text;
+
+    void OnEnable()
+    {
+        m_Text = GetComponent<Text>();
+        int score = 10;
+        m_Text.text = "Score: " + score;
+    }
+
+}
---
+++ Assets/TextSizeScaler.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextSizeScaler : MonoBehaviour
+{
+    public int maxFontSize = 20;
+    public int minFontSize = 12;
+
+    Text m_Text;
+
+    void Start()
+    {
+        m_Text = GetComponent<Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float t = (Mathf.Sin(Time.time * 8.0f) * 0.5f + 1);
+        m_Text.fontSize = (int)Mathf.Lerp(minFontSize, maxFontSize, t);
+    }
+}
